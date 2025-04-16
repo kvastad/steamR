a7d47fb1c7ebf56cd8025b8a1e5d62c5b4885af1
Rasool Saghaleyni <rasool.saghaleyni@gmail.com>
2025-04-16 10:49:12 +0200

    Add load_permutation_matrices function

---
+++ R/load_permutation_matrices.R
@@ -0,0 +1,17 @@
+#' Load Permutation Matrices from RDS Files
+#'
+#' Loads a list of permutation matrix `.rds` files and returns them as a named list of data frames or matrices.
+#'
+#' @param file_paths A character vector of file paths pointing to `.rds` files containing permutation matrices.
+#'
+#' @returns A named list of permutation matrices, where names correspond to the file base names.
+#' @export
+#'
+#' @examples
+#' file_paths <- list.files("data/permutations", pattern = "\\.rds$", full.names = TRUE)
+#' permutation_matrices <- load_permutation_matrices(file_paths)
+load_permutation_matrices <- function(file_paths) {
+  matrices <- lapply(file_paths, readRDS)
+  names(matrices) <- basename(file_paths)
+  return(matrices)
+}
